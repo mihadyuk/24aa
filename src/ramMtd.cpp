d60e26b6b02c1403b80ff54cb88da54788a4ea00
Andrey Mihadyuk <mihadyuk@ntlab.com>
2015-03-18 17:50:35 +0300

    Added ram mtd class just for test purposes.

---
+++ src/ramMtd.cpp
@@ -0,0 +1,65 @@
+/*
+ * ramMtd.cpp
+ *
+ *  Created on: Mar 18, 2015
+ *      Author: user
+ */
+
+#include "ramMtd.h"
+#include <string.h>
+
+namespace nvram {
+
+RamMtd::RamMtd(uint8_t *bufferStorage, size_t storageSize) :
+    Mtd(nullptr),
+    m_bufferStorage(bufferStorage),
+    m_storageSize(storageSize)
+{
+  // TODO Auto-generated constructor stub
+
+}
+
+RamMtd::~RamMtd() {
+  // TODO Auto-generated destructor stub
+}
+
+msg_t RamMtd::read(uint8_t *data, size_t len, size_t offset) {
+
+    osalDbgAssert((offset + len) <= capacity(), "Transaction out of device bounds");
+    if ((offset + len) > capacity())
+      return MSG_RESET;
+
+    Mtd::acquire();
+    memcpy(data, &m_bufferStorage[offset], len);
+    Mtd::release();
+
+    return MSG_OK;
+
+}
+
+msg_t RamMtd::write(const uint8_t *data, size_t len, size_t offset) {
+
+  osalDbgAssert((offset + len) <= capacity(), "Transaction out of device bounds");
+  if ((offset + len) > capacity())
+    return MSG_RESET;
+
+  Mtd::acquire();
+  memcpy(&m_bufferStorage[offset], data, len);
+  Mtd::release();
+
+  return MSG_OK;
+
+}
+
+msg_t RamMtd::shred(uint8_t pattern) {
+  (void)pattern;
+  return MSG_OK;
+}
+
+size_t RamMtd::capacity(void) {
+  return m_storageSize;
+}
+
+
+
+} /* namespace nvram_fs */
---
+++ src/ramMtd.h
@@ -0,0 +1,29 @@
+/*
+ * ramMtd.h
+ *
+ *  Created on: Mar 18, 2015
+ *      Author: user
+ */
+
+#ifndef EEPROM_SRC_RAMMTD_H_
+#define EEPROM_SRC_RAMMTD_H_
+#include "mtd.hpp"
+
+namespace nvram {
+
+  class RamMtd : public Mtd {
+  public:
+    RamMtd(uint8_t *bufferStorage, size_t storageSize);
+    ~RamMtd();
+    msg_t read(uint8_t *data, size_t len, size_t offset) override;
+    msg_t write(const uint8_t *data, size_t len, size_t offset) override;
+    msg_t shred(uint8_t pattern) override;
+    size_t capacity(void) override;
+  private:
+    uint8_t *m_bufferStorage = nullptr;
+    size_t m_storageSize     = 0;
+  };
+
+} /* namespace nvram_fs */
+
+#endif /* EEPROM_SRC_RAMMTD_H_ */
